5a066959048f6a5f3f15824b8c9b946c9b1cb8a3
lee <hagulu@naver.com>
2022-02-17 18:03:29 +0900

    [이강산] - spring 문제 풀이 -Mybatis 문제 풀이

---
+++ src/main/java/com/ashspell/ex/lesson03/dao/ReviewDAO.java
@@ -0,0 +1,15 @@
+package com.ashspell.ex.lesson03.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.ashspell.ex.lesson03.model.Review;
+
+@Repository
+public interface ReviewDAO {
+	
+	// id 값을 전달 받아서 해당하는 리뷰 값을 리턴한다
+	public Review selectReview(@Param("id") int id);
+		
+	
+}
